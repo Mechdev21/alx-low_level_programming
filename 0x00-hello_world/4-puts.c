6b78b18e1538fb7168bb54ff2acdad11e322eef6
Orji Favour <favour@titanname.com>
2022-12-12 12:03:45 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Using puts print: programmin
+ * is like building a multilingual puzzle
+ *
+ * Return 0
+ */
+ int main(void)
+ {
+	 puts("\"Programming is like building a multilingual puzzle\n");
+	 return (0);
+ }
