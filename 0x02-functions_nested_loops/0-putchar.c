77d920f2cc01a79218c90faad136df6160938aef
Orji Favour <favour@titanname.com>
2022-12-17 05:06:57 -0800

    using _putchar to print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * Main - Entry point prints _putchar followed by newline
+ * Description: A program that prints _putchar
+ *
+ * Return: on success 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char s[9] = "_putchar";
+
+	while (i < 9)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
