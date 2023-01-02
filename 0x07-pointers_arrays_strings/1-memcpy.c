78f41717b92a5ce7c3d38382f1d354768df3c37e
Orji Favour <favour@titanname.com>
2023-01-02 07:16:53 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,12 +12,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	if ((src != NULL) && (dest != NULL))
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			src[i] = dest[i];
-		}
+		dest[i] = src[i];
 	}
 	return (dest);
 }
