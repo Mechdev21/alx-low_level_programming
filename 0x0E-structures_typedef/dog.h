ccad2437e4af38ebd8dfe6b783cb980a1006b3c2
Orji Favour <favour@titanname.com>
2023-01-16 10:16:44 -0800

    structure vcalled dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines a new type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: who owns the dpog
+ *
+ * Description: used to create a custom data type called dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
