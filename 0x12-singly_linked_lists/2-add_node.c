ea2841ffc393535e25e9578f00fad58fe728f7b2
Orji Favour <favour@titanname.com>
2023-03-16 04:10:21 -0700

    list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,17 +13,21 @@ list_t *add_node(list_t **head, const char *str)
 {
 
 	list_t *new;
-    size_t len = 0;
+    unsigned int len = 0;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
 
-    if (str != NULL)
+    if (str == NULL)
     {
-	    while (str[len])
-		    len++;
+        new->str = NULL;
+        new->len = 0;
+        new->next = *head;
     }
-    len++;
-	new = malloc(sizeof(list_t));
-	if (!new)
-		return (NULL);
+    while (str[len])
+    {
+        len++;
+    }
 
 	new->str = strdup(str);
 	new->len = len;
