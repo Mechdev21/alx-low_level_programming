f002833ab2be8ec427ce3f789372cf7516d85adc
favour orji <favour@titanname.com>
2023-07-23 12:39:11 -0700

    calc

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - performs addition
+ * @a: values
+ * @b: values
+ *
+ * Return: additon of both
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - performs subtraction
+ * @a: value
+ * @b: value
+ *
+ * Return: subtraction of both
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - performs multiplication
+ * @a: value
+ * @b: value
+ *
+ * Return: multiplication of both
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - peforms division
+ * @a: value
+ * @b: value
+ *
+ * Return: division of both
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulus operation
+ * @a: value
+ * @b: value
+ *
+ * Return: modulus of a b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
