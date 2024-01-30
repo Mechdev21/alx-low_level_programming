093b873e236a3e86ed1976364fe6eaaaea0f3227
Orji Favour <favour@titanname.com>
2024-01-29 18:17:37 -0800

    task

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - adds two int
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub two int
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - mul two int
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - div two int
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - mod two int
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+
+int mod(int a, int b)
+{
+        return (a % b);
+}
