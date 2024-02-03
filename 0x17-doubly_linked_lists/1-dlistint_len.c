86f00223781ab37fda83c3bd2fdb0d3eca56f787
Orji Favour <favour@titanname.com>
2024-02-03 11:41:49 -0800

    task

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that returns length of a list
+ * h: function pointer to head
+ *
+ * Return: Leght of the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+	const dlistint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		n += 1;
+	}
+	return (n);
+}
