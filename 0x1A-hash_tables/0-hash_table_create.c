ec3f1c2e1f30eee40249530309aad791207bb68e
Orji Favour <favour@titanname.com>
2024-02-16 20:07:26 -0800

    hashtable

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - A function used to create a hash table
+ * @size: tghe size of the table
+ *
+ * Return:  a pointer to the newly created hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+
+	return (table);
+}
